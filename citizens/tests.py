3e31dc2a754cc84df4f93458d9d5774088d2d37d
murnat98 <murnat98@yandex.ru>
2019-08-22 19:57:38 +0400

    tests for change citizens written

--- citizens/tests.py
+++ citizens/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from backend_school import settings
+from citizens.models import Citizens, Relatives
 
 
 class ImportsTest(TestCase):
@@ -35,7 +36,7 @@ class ImportsTest(TestCase):
         """
         return os.listdir(os.path.join(settings.BASE_DIR, 'citizens', 'test_data', 'wrong_data'))
 
-    def test_wrong_data(self):
+    def test_wrong_data(self):  # TODO: check database info
         test_files = self.get_wrong_files_list()
 
         for test_file in test_files:
@@ -50,3 +51,73 @@ class ImportsTest(TestCase):
         self.assertIsInstance(content, dict)
         self.assertIn('data', content)
         self.assertIn('import_id', content['data'])
+
+
+class ChangeCitizensTest(TestCase):
+    """
+    TODO: optimize!
+    """
+    def setUp(self):
+        response = self.client.post(reverse('citizens:imports'),
+                                    ImportsTest.get_data_from_file('test_right_data.json'),
+                                    content_type='application/json')
+
+        self.import_id = response.content['data']['import_id']
+
+    def test_fields(self):
+        response = self.client.patch(
+            reverse('citizens:change_imports', kwargs={'import_id': self.import_id, 'citizen_id': 2}),
+            {'town': 'Керчь', 'apartment': 19}
+        )
+
+        content = json.loads(response.content)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('data', content)
+
+        citizen = content['data']
+
+        self.assertIn('town', citizen)
+        self.assertEqual(citizen['town'], 'Керчь')
+        self.assertIn('apartment', citizen)
+        self.assertEqual(citizen['apartment'], 19)
+        self.assertIn('citizen_id', citizen)
+        self.assertEqual(citizen['citizen_id'], 2)
+        self.assertIn('birth_date', citizen)
+        self.assertEqual(citizen['birth_date'], '26.12.1986')
+
+        citizen_instance = Citizens.objects.get(import_id=self.import_id, citizen_id=2)
+
+        self.assertEqual(citizen_instance.town, 'Керчь')
+        self.assertEqual(citizen_instance.apartment, 19)
+        self.assertEqual(citizen_instance.name, 'Иванов Иван Иванович')
+
+    def test_relatives(self):
+        new_relatives = (3, 4)
+
+        response = self.client.patch(
+            reverse('citizens:change_imports', kwargs={'import_id': self.import_id, 'citizen_id': 2}),
+            {'relatives': new_relatives}
+        )
+
+        content = response.content
+
+        self.assertEqual(content['data']['relatives'], new_relatives)
+
+        citizen = Citizens.objects.get(import_id=self.import_id, citizen_id=2)
+
+        relatives = Relatives.objects.filter(import_id=self.import_id, citizen_1_id=citizen.id)
+        relatives |= Relatives.objects.filter(import_id=self.import_id, citizen_2_id=citizen.id)
+
+        for relative in relatives:
+            citizen_1_id = relative.citizen_1_id
+            citizen_2_id = relative.citizen_2_id
+
+            relative_id = 0
+
+            if citizen_1_id == citizen.id:
+                relative_id = citizen_2_id
+            else:
+                relative_id = citizen_1_id
+
+            self.assertIn(relative_id, new_relatives)
